a81aa0b28b1ff2f895894548bd06b4f67626aa55
Keri Barrera <keriman@gmail.com>
2013-08-14 20:49:29 -0500

    Ultimo?
    Last commit

--- consulta.php
+++ consulta.php
@@ -1,44 +1,45 @@
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title></title>
-		<link rel="stylesheet" href="themes/tema.min.css" />
-		<link rel="stylesheet" href="themes/jquery.mobile.structure-1.3.2.min.css" />
-		<script src="themes/jquery-1.9.1.min.js"></script>
-		<script src="themes/jquery.mobile-1.3.2.min.js"></script>
-	</head>
-	<body>
-		<div data-role="page" data-theme="a">
-			<div data-role="header" data-position="inline">
-				<h3>Hospital Médico Siglo XXI</h3>
-			</div>
-			<div data-role="content" data-theme="a">
-				<?php 
-$conexion=mysql_connect("mysql.hostinger.mx","u325210545_root","Class.1") or die ("No se pudo conectar");
-	mysql_select_db("u325210545_quiro", $conexion) or die ("No se puede seleccionar la base de datos");
-	
-	if(isset($_POST["texto"])){
-		$texto=$_POST["texto"];
-		$result=mysql_query("SELECT nombre,nss FROM pacientes WHERE nss='$texto'" );
-		$dato=mysql_fetch_array($result);
+<?php 
+$conexion=mysql_connect("localhost","root","class") or die ("No se pudo conectar");
+	mysql_select_db("cirugias", $conexion) or die ("No se puede seleccionar la base de datos");
 
-	if ($dato==""){
-		echo "Ninguna cirugia pendiente!!! <br />";
-	}
-		else{
-				echo "CIRUGIA PENDIENTE";
-				echo $result;
+//if(isset($_POST['a'])){
+$id=1;//$_POST['a'];
+//}
+$consulta=mysql_query("SELECT p.nombre, p.nss,p.padecimiento, c.idquirofano as num_quirofano, c.fechainicio, q.especificaciones FROM pacientes as p, cirugia as c, quirofano as q where p.idpaciente='$id' and p.idcirugia=c.idcirugia and c.idquirofano=q.idquirofano");
+if($id=mysql_fetch_array($consulta)){
+	echo "NSS ENCONTRADO! <br/>";
+?>	
+<table width='100' border='1'>
+		  <tr>
+			<td>Paciente</td>
+			<td><?php echo $id['nombre'];?></td>
+		  </tr>
+		  <tr>
+			<td>NSS</td>
+			<td><?php echo $id['nss'];?></td>
+		  </tr>
+		  <tr>
+			<td>Padecimiento</td>
+			<td><?php echo $id['padecimiento'];?></td>
+		  </tr>
+		  <tr>
+			<td>Fecha</td>
+			<td><?php echo $id['fechainicio'];?></td>
+		  </tr>
+		  <tr>
+			<td>Quirofano</td>
+			<td><?php echo $id['idquirofano'];?></td>
+		  </tr>
+          <tr>
+			<td>Tipo</td>
+			<td><?php echo $id['especificaciones'];?></td>
+		  </tr>
+		</table>
+
+<?php
+	}else{
+		echo "id no existe!";
 		}
 
-											
-	}
 	
 ?>
-<a href="index.html">
-	<input name="volver" type="button" value="Volver" />
-</a>
-			</div>
-		</div>
-	</body>
-</html>
